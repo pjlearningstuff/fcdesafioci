c75e8d51ea6c32809e9a94a171fe320ad00ed33a
pedrojpx <pjpxavier@gmail.com>
2023-12-27 14:33:49 -0300

    feat: new conversions

--- src/temperature.js
+++ src/temperature.js
@@ -6,4 +6,20 @@ function fahr2Celsius(f) {
     return (f - 32) * 5/9
 }
 
+function celsius2Kelvin(c) {
+    return c + 273.15
+}
+
+function kelvin2Celsius(k) {
+    return k - 273.15
+}
+
+function fahr2Kelvin(f) {
+    return (f - 32) * 5/9 + 273.15
+}
+
+function kelvin2Fahr(k) {
+    return (k - 273.15) * 9/5 + 32
+}
+
 module.exports = { celsius2Fahr, fahr2Celsius }
