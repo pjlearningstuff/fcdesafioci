865d967c4e57f7016cc45e54a8bd2af6ddfc40c7
pedrojpx <pjpxavier@gmail.com>
2023-12-27 14:49:22 -0300

    added more tests

--- src/temperature.test.js
+++ src/temperature.test.js
@@ -1,9 +1,17 @@
-const {celsius2Fahr, fahr2Celsius} = require('./temperature')
+const { celsius2Fahr, fahr2Celsius, celsius2Kelvin, kelvin2Celsius, fahr2Kelvin, kelvin2Fahr} = require('./temperature')
 
 test("convert 0C to be equal 32F", () => {
-    expect(celsius2Fahr(0)).toBe(32)
+    expect(celsius2Fahr(0)).toBeCloseTo(32)
 })
 
-//test("convert 32F to be 0C", () => {
-//    expect(fahr2Celsius(32)).toBe(0)
-//})
+test("convert 32F to be 0C", () => {
+    expect(fahr2Celsius(32)).toBeCloseTo(0)
+})
+
+test("convert 0C to -273.15K", () => {
+    expect(celsius2Kelvin(0)).toBeCloseTo(-273.15)
+})
+
+test("convert -273.15K to 0C", () => {
+    expect(kelvin2Celsius(-273.15)).toBeCloseTo(0)
+})
